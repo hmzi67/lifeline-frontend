652966820b36b17bb529d6ede5daac273eaf92e4
hmzi67 <hamzawaheed057@gmail.com>
2025-06-15 14:48:34 +0500

    feat: pages created and routing configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Button } from './components/ui/button'
+import Layout from './components/common/Layout'
+import Landing from './pages/marketing/Landing'
+import Business from './pages/marketing/Business'
+import Blog from './pages/content/Blog'
+import BlogReading from './pages/content/BlogReading'
+import Login from './pages/auth/Login'
+import Dashboard from './pages/dashboard/Dashboard'
+import Goals from './pages/fitness/Goals'
+import Analytics from './pages/fitness/Analytics'
+import Pricing from './pages/marketing/Pricing'
+import Contact from './pages/marketing/Contact'
+import Error from './pages/utility/Error'
 
 function App() {
 
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/business" element={<Business />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogReading />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/goals" element={<Goals />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/error" element={<Error />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Layout>
+      </Router>
     </>
   )
 }
