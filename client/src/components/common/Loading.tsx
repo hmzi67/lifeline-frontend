6a5a4993a5a5d121c46016abd22ddd65b34ea7f8
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-07-24 22:18:08 +0500

    feat(frontend): Adding Animations In Loading Component

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {LoaderCircle} from "lucide-react";
 
 const Loading: React.FC = () => {
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-900">
-            <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+        <div className="flex flex-col gap-2 items-center justify-center h-screen bg-gray-300">
+            <LoaderCircle className="w-12 h-12 text-primary-800 animate-spin" />
+            <p className={'text-lg text-gray-900'}>Loading</p>
         </div>
     );
 };
