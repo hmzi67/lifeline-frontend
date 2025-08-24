503e23d3152de8829327102bebe19f2f34fe6f7d
Muhammad Naveed <naveed@codehuntspk.com>
2025-08-24 20:13:56 +0500

    refactor: Remove onBack prop from ThankYouCard and its usage in Questions component

--- client/src/pages/marketing/Questions.tsx
+++ client/src/pages/marketing/Questions.tsx
@@ -138,7 +138,6 @@ export default function Questions() {
     <ThankYouCard
       key="ThankYouCard1"
       onComplete={() => goToNext(gender)}
-      onBack={() => goToPrevious()}
     />,
 
     <FitnessLevelSelector
